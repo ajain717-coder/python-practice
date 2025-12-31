d1811037d8bd27a35f944f0386d5a027b86b3a09
AAYUSH JAIN <ajain717@gmail.com>
2025-12-31 16:33:08 +0530

    Assign3 task1 and task2

---
+++ Assign3_task1_fact.py
@@ -0,0 +1,7 @@
+n=int(input('enter the no.:'))
+k=1
+for i in range(1,n+1):
+    k=k*i
+print(f"Factorial of {n} is : {k}")
+
+
---
+++ Assign3_task2_maths.py
@@ -0,0 +1,9 @@
+import math
+a= int(input("Enter the first number:"))
+c=math.sqrt(a)
+d=math.log(a)
+e=math.sin(a)
+print(f"Square root:{c}")
+print(f"Logarithm:{d}")
+print(f"Sine:{e}")
+25
